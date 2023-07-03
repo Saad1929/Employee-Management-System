7b10ccc7d2989f183ef2a96a814029c39facb21c
Saad1929 <saad2baig123@gmail.com>
2023-07-03 21:19:13 +0100

    POST Request Backend

--- fullstack-backend/src/main/java/com/saadbaig/fullstackbackend/controller/UserController.java
+++ fullstack-backend/src/main/java/com/saadbaig/fullstackbackend/controller/UserController.java
@@ -3,9 +3,12 @@ package com.saadbaig.fullstackbackend.controller;
 import com.saadbaig.fullstackbackend.model.User;
 import com.saadbaig.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -17,5 +20,11 @@ public class UserController {
     User newUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
     }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
 
 }
