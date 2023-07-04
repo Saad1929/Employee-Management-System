48d570b00f53083eb05a72f6485a038c839c71e7
Saad1929 <saad2baig123@gmail.com>
2023-07-04 21:47:20 +0100

    Frontend Config Settings

--- fullstack-backend/src/main/java/com/saadbaig/fullstackbackend/controller/UserController.java
+++ fullstack-backend/src/main/java/com/saadbaig/fullstackbackend/controller/UserController.java
@@ -3,14 +3,12 @@ package com.saadbaig.fullstackbackend.controller;
 import com.saadbaig.fullstackbackend.model.User;
 import com.saadbaig.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
