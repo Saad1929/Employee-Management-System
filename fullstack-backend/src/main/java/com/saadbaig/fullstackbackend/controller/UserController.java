ede344c10e3dca2f8249e13351f37b61cdcb0e01
Saad1929 <saad2baig123@gmail.com>
2023-07-05 20:26:35 +0100

    Update Backend Working

--- fullstack-backend/src/main/java/com/saadbaig/fullstackbackend/controller/UserController.java
+++ fullstack-backend/src/main/java/com/saadbaig/fullstackbackend/controller/UserController.java
@@ -31,6 +31,18 @@ public class UserController {
                 .orElseThrow(() -> new UserNotFoundException(id));
     }
 
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(() ->new UserNotFoundException(id));
+    }
+
+
 
 
 }
